d2e23690aedad90c999ad11eb48035223422de8b
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2023-10-25 10:11:04 +1300

    Update mullvad.sh

--- Mullvad/mullvad.sh
+++ Mullvad/mullvad.sh
@@ -1,7 +1,15 @@
+# For Unix-like systems, tested on Linux (Kernel 6.5.8)
+# Connects to Mullvad VPN server nz-akl-wg-301
+################################################
+# Requires:
+# 1. curl
+# 2. wireguard (with /etc/wireguard as a valid path, and wg-quick as a valid terminal command)
+# 3. A valid internet connection
+# 4. super user permissions
+
 echo Please choose either:
-echo 1. Connect to Mullvad service
-echo 2. Disconnect from Mullvad service
-echo now:
+echo 1. Connect to Mullvad service (Wireguard)
+echo 2. Disconnect from Mullvad service (Wireguard)
 read option
 
 if [ $option -eq 1 ]
